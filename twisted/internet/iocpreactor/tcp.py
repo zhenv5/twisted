f45d72d9ec167a067c3d239f596d88834ecca5c5
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-02-17 20:13:55 +0000

    copy the comment from the other tcp.py :/
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/connector-buildProtocol-710@41706 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/iocpreactor/tcp.py
+++ twisted/internet/iocpreactor/tcp.py
@@ -291,8 +291,12 @@ class Client(_BaseBaseClient, _BaseTCPClient, Connection):
             logPrefix = self._getLogPrefix(self.protocol)
             self.logstr = logPrefix + ",client"
             if self.protocol is None:
-                # Make up a protocol to satisfy the rest of the implementation;
-                # connectionLost is going to be called, for example.
+                # Factory.buildProtocol is allowed to return None.  In that
+                # case, make up a protocol to satisfy the rest of the
+                # implementation; connectionLost is going to be called on
+                # something, for example.  This is easier than adding special
+                # case support for a None protocol throughout the rest of the
+                # transport implementation.
                 self.protocol = Protocol()
                 # But dispose of the connection quickly.
                 self.loseConnection()
