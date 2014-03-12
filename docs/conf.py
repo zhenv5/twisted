c6ed590a67f8aa15ba0549aca1d601a36ed77230
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-03-12 00:28:45 +0000

    Oops, reverse bogus changes in conf.py
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tubes-1956-5@41865 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- docs/conf.py
+++ docs/conf.py
@@ -116,12 +116,9 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-# if not on_rtd:
-#     html_theme = 'twistedtrac'
+if not on_rtd:
+    html_theme = 'twistedtrac'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -129,7 +126,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = ['_themes']
+html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
