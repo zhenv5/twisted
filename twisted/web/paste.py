b789f51cf4bb75f34b4c945f7c7c480fe7d75216
tomprince <tomprince@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-04-15 18:11:00 +0000

    Implement the server.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/paste-deploy-6595@42267 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/web/paste.py
+++ twisted/web/paste.py
@@ -5,7 +5,35 @@
 Integration with L{paste.deploy}
 """
 
-def serverFactory(globalConfig, ports="tcp:8080", reactor=None):
+from twisted.internet.endpoints import serverFromString
+from twisted.application.reactors import installReactor
+from twisted.python.threadpool import ThreadPool
+
+
+class _PasteServer(object):
+    def __init__(self, reactorName, ports):
+        self.ports = ports
+        self.reactorName = reactorName
+    def __call__(self, app):
+        from twisted.web.server import Site
+        from twisted.web.wsgi import WSGIResource
+        reactor = installReactor(self.reactorName)
+
+        threads = ThreadPool()
+        threads.start()
+
+        resource = WSGIResource(reactor, threads, app)
+        site = Site(resource)
+
+        for port in self.ports:
+            endpoint = serverFromString(reactor, port)
+            endpoint.listen(site)
+
+        reactor.run()
+
+
+
+def serverFactory(globalConfig, ports=b"tcp:8080", reactor=None):
     """
     Creates a WSGI server for use with L{paste.deploy}.
 
@@ -18,3 +46,4 @@ def serverFactory(globalConfig, ports="tcp:8080", reactor=None):
     @param reactor: Name of reactor to use.
     @type reactor: L{str}
     """
+    return _PasteServer(reactor, ports.split(b' '))
