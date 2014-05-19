f3f46bd96361848adeb0d376a07e8c0d8b84e9c3
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-05-19 18:23:37 +0000

    Change example to talk to Docker.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/agent-endpointFactory-6634-2@42664 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- docs/web/howto/listings/client/endpointconstructor.py
+++ docs/web/howto/listings/client/endpointconstructor.py
@@ -1,43 +1,36 @@
+"""
+Send a HTTP request to Docker over a Unix socket.
+
+May need to be run as root.
+"""
 from __future__ import print_function
 
 from sys import argv
 
-from twisted.internet.endpoints import TCP4ClientEndpoint, SSL4ClientEndpoint
+from twisted.internet.endpoints import UNIXClientEndpoint
 from twisted.internet.task import react
-from twisted.web.client import Agent
-from twisted.web.http_headers import Headers
+from twisted.web.client import Agent, readBody
 
 
-
-class EndpointConstructor(object):
+class DockerEndpointConstructor(object):
+    """
+    Connect to Docker's Unix socket.
+    """
     def __init__(self, reactor):
         self.reactor = reactor
 
 
-    def constructEndpoint(
-            self, scheme, host, port, httpsConnectionCreator):
-        print('Creating an endpoint:',
-              scheme, host, port, httpsConnectionCreator)
-        if scheme == b'http':
-            return TCP4ClientEndpoint(self.reactor, host, port)
-        elif scheme == b'https':
-            if httpsConnectionCreator is None:
-                raise NotImplementedError('TLS support unavailable')
-            return SSL4ClientEndpoint(
-                self.reactor, host, port, httpsConnectionCreator)
-        else:
-            raise ValueError("Unsupported scheme: %r" % (scheme,))
+    def constructEndpoint(self, scheme, host, port):
+        return UNIXClientEndpoint(self.reactor, "/var/run/docker.sock")
 
 
 
-def main(reactor, url=b"http://example.com/"):
-    agent = Agent(reactor, endpointConstructor=EndpointConstructor(reactor))
-    d = agent.request(
-        'GET', url,
-        Headers({'User-Agent': ['Twisted Web Client Example']}))
-    d.addCallback(print, 'Response received')
+def main(reactor, path=b"/containers/json?all=1"):
+    agent = Agent.forEndpointConstructor(
+        reactor, DockerEndpointConstructor(reactor))
+    d = agent.request(b'GET', b"unix://localhost" + path)
+    d.addCallback(readBody)
+    d.addCallback(print)
     return d
-
-
 
 react(main, argv[1:])
