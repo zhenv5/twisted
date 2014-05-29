8f1c760aeda44d7a5f52fa01337aea89354c97f6
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-05-29 00:08:32 +0000

    Better, more useful portforward example.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tubes-1956-6@42750 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- docs/core/howto/listings/tubes/portforward.py
+++ docs/core/howto/listings/tubes/portforward.py
@@ -1,19 +1,21 @@
+
+import os
+
 from twisted.tubes.protocol import factoryFromFlow
-from twisted.internet.endpoints import TCP4ServerEndpoint
+from twisted.internet.endpoints import serverFromString, clientFromString
 from twisted.internet.defer import Deferred
-from twisted.internet.endpoints import TCP4ClientEndpoint
 
-def main(reactor, host="localhost", port="4321", localport="6543"):
-    clientEndpoint = TCP4ClientEndpoint(reactor, host, int(port))
-    serverEndpoint = TCP4ServerEndpoint(reactor, int(localport))
+def main(reactor, listen="tcp:4321", connect="tcp:localhost:6543"):
+    clientEndpoint = clientFromString(reactor, connect)
+    serverEndpoint = serverFromString(reactor, listen)
 
-    def forwardTubeFactory(listeningFount, listeningDrain):
-        def outgoing(connectingFount, connectingDrain):
+    def incomingTubeFactory(listeningFount, listeningDrain):
+        def outgoingTubeFactory(connectingFount, connectingDrain):
             listeningFount.flowTo(connectingDrain)
             connectingFount.flowTo(listeningDrain)
-        clientEndpoint.connect(factoryFromFlow(outgoing))
+        clientEndpoint.connect(factoryFromFlow(outgoingTubeFactory))
 
-    serverEndpoint.listen(factoryFromFlow(forwardTubeFactory))
+    serverEndpoint.listen(factoryFromFlow(incomingTubeFactory))
     return Deferred()
 
 from twisted.internet.task import react
