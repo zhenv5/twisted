34396b9996d49b053cc9cb4239a0e7c33be60da8
glyph <glyph@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2014-12-11 01:44:22 +0000

    correct test module name
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/tubes-1956-8@43613 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/tubes/test/test_kit.py
+++ twisted/tubes/test/test_kit.py
@@ -1,4 +1,4 @@
-# -*- test-case-name: twisted.tube.test.test_pauser -*-
+# -*- test-case-name: twisted.tube.test.test_kit -*-
 # Copyright (c) Twisted Matrix Laboratories.
 # See LICENSE for details.
 
