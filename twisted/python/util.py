96602ff9b6464f3da5230d233038f3c3b7586772
hawkowl <hawkowl@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2015-06-29 15:11:22 +0000

    merging forward
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/trialrunner-py3-5965-4@45136 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/util.py
+++ twisted/python/util.py
@@ -1046,8 +1046,7 @@ __all__ = [
 
 
 if _PY3:
-    __notported__ = ["SubclassableCStringIO", "LineLog", "spewer",
-                     "makeStatBar"]
+    __notported__ = ["SubclassableCStringIO", "LineLog", "makeStatBar"]
     for name in __all__[:]:
         if name in __notported__:
             __all__.remove(name)
